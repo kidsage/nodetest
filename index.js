d51b02375693456ad8b6e768e3381f91faa18b85
Ian Kim <ruhyun92@gmail.com>
2023-10-14 03:54:44 +0900

    + Day 3

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const app = express()
+const port = 3000
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.get('/', (req, res) => {
+    res.send('Hello World')
 })
 
-app.listen(3000)
+app.get('/dog', (req, res) => {
+    res.json({'sound': '멍멍'})
+})
+
+app.get('/cat', (req, res) => {
+    res.send('고양이')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
